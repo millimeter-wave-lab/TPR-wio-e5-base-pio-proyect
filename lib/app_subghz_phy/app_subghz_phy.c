06a82d38adb1164b4b7f1e9d38abbf3a412cac22
srFelipes <37315690+srFelipes@users.noreply.github.com>
2023-05-19 14:28:04 -0400

    initialization of the radio with default parameters

--- lib/app_subghz_phy/app_subghz_phy.c
+++ lib/app_subghz_phy/app_subghz_phy.c
@@ -22,9 +22,11 @@
 #include "app_subghz_phy.h"
 #include "subghz_phy_app.h"
 #include "sys_app.h"
+#include "radio.h"
+#include "usart.h"
 
 /* USER CODE BEGIN Includes */
-
+#include "radio_def.h"
 /* USER CODE END Includes */
 
 /* External variables ---------------------------------------------------------*/
@@ -39,17 +41,46 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
+#define F_868MHz                      868000000
+#define F_915MHz                      915000000
+#define P_14dBm                       14
+#define P_22dBm                       22
+#define SF12                          12
+#define CR4o5                         1
+#define EMISSION_POWER                P_14dBm
+#define CONTINUOUS_TIMEOUT           0xFFFF
+#define LORA_PREAMBLE_LENGTH          8         /* Same for Tx and Rx */
+#define LORA_SYMBOL_TIMEOUT           30        /* Symbols */
+#define TX_TIMEOUT_VALUE              3000
+#define LORA_FIX_LENGTH_PAYLOAD_OFF   false
+#define LORA_IQ_INVERSION_OFF         false
+#define TX_TEST_TONE                  (1<<0)
+#define RX_TEST_RSSI                  (1<<1)
+#define TX_TEST_LORA                  (1<<2)
+#define RX_TEST_LORA                  (1<<3)
+#define RX_TIMEOUT_VALUE              5000
+#define RX_CONTINUOUS_ON              1
+#define PRBS9_INIT                    ( ( uint16_t) 2 )
+#define DEFAULT_PAYLOAD_LEN           16
+#define DEFAULT_LDR_OPT               2
+#define DEFAULT_FSK_DEVIATION         25000
+#define DEFAULT_GAUSS_BT              3 /*Lora default in legacy*/
 
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
 /* USER CODE BEGIN PM */
+void OnRxDone(uint8_t *payload, uint16_t size, int16_t rssi, int8_t LoraSnr_FskCfo){
+  uint8_t msg[] = "Received a message";
+  HAL_UART_Transmit(&huart1, msg, sizeof(msg), 19);
 
+}
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
 /* USER CODE BEGIN PV */
-
+static testParameter_t testParam = { TEST_LORA, F_915MHz, EMISSION_POWER, BW_125kHz, SF12, CR4o5, 0, 0, DEFAULT_PAYLOAD_LEN, DEFAULT_FSK_DEVIATION, DEFAULT_LDR_OPT, DEFAULT_GAUSS_BT};
+RxConfigGeneric_t RxConfig;
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -70,7 +101,19 @@ void MX_SubGHz_Phy_Init(void)
   /* USER CODE END MX_SubGHz_Phy_Init_1_1 */
   SubghzApp_Init();
   /* USER CODE BEGIN MX_SubGHz_Phy_Init_2 */
-
+  RxConfig.lora.Bandwidth = (RADIO_LoRaBandwidths_t) testParam.bandwidth;
+  RxConfig.lora.SpreadingFactor = (RADIO_LoRaSpreadingFactors_t) testParam.loraSf_datarate; /*BitRate*/
+  RxConfig.lora.Coderate = (RADIO_LoRaCodingRates_t)testParam.codingRate;
+  RxConfig.lora.LowDatarateOptimize = (RADIO_Ld_Opt_t)testParam.lowDrOpt; /*0 inactive, 1 active, 2: auto*/
+  RxConfig.lora.PreambleLen = LORA_PREAMBLE_LENGTH;
+  RxConfig.lora.LengthMode = RADIO_LORA_PACKET_VARIABLE_LENGTH;
+  RxConfig.lora.CrcMode = RADIO_LORA_CRC_ON;
+  RxConfig.lora.IqInverted = RADIO_LORA_IQ_NORMAL;
+  Radio.RadioSetRxGenericConfig(GENERIC_LORA, &RxConfig, RX_CONTINUOUS_ON, LORA_SYMBOL_TIMEOUT);
+  Radio.Rx(0);
+  uint8_t msg[] = "finished initializating lora radio";
+  HAL_UART_Transmit(&huart1,msg,sizeof(msg),10);
+  // HAL_UART_Transmit(&huart2,msg,sizeof(msg),10);
   /* USER CODE END MX_SubGHz_Phy_Init_2 */
 }
 
@@ -90,5 +133,6 @@ void MX_SubGHz_Phy_Process(void)
 
 /* Private Functions Definition -----------------------------------------------*/
 /* USER CODE BEGIN PrFD */
+
 
 /* USER CODE END PrFD */
--- lib/app_subghz_phy/app_subghz_phy.h
+++ lib/app_subghz_phy/app_subghz_phy.h
@@ -28,16 +28,48 @@ extern "C" {
 
 /* Includes ------------------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include <stdint.h>
 /* USER CODE END Includes */
 
 /* Exported types ------------------------------------------------------------*/
 /* USER CODE BEGIN ET */
 
+typedef enum
+{
+  BW_7kHz = 0,
+  BW_12kHz = 1,
+  BW_31kHz = 2,
+  BW_62kHz = 3,
+  BW_125kHz = 4,
+  BW_250kHz = 5,
+  BW_500kHz = 6,
+} Lora_BandWidth_t;
+
+
+typedef struct
+{
+  uint32_t modulation;         /* 0: FSK, 1: Lora, 2:BPSK(Tx) */
+  uint32_t freq;               /* in Hz */
+  int32_t power;               /* [-9 :22]dBm */
+  uint32_t bandwidth;          /* Lora [0:7.8125, 1: 15.625, 2: 31.25, 3: 62.5, 4: 125, 5: 250, 6: 500]kHz
+                                  FSK : [4800Hz :467000 Hz] */
+  uint32_t loraSf_datarate;    /* Lora[SF5..SF12] FSK [600..300000 bits/s] */
+  uint32_t codingRate;         /* Lora Only [1: 4/5, 2: 4/6, 3: 4/7, 4: 4/8] */
+  uint32_t lna;                /* 0:off 1:On */
+  uint32_t paBoost;            /* 0:off 1:On */
+  uint32_t payloadLen;         /* [1:256] */
+  uint32_t fskDev;             /* FSK only [4800:467000]
+                                  Note: no check applied wrt bandwidth. Common practice is to have bandwidth>1,5*fskDev */
+  uint32_t lowDrOpt;           /* Lora Only 0: off, 1:On, 2: Auto (1 when SF11 or SF12, 0 otherwise) */
+  uint32_t BTproduct;          /* FSK only [0 no Gaussian Filter Applied, 1: BT=0,3, 2: BT=0,5, 3: BT=0,7, 4: BT=1] */
+} testParameter_t;
 /* USER CODE END ET */
 
 /* Exported constants --------------------------------------------------------*/
 /* USER CODE BEGIN EC */
+#define TEST_FSK                           0
+#define TEST_LORA                          1
+#define TEST_BPSK                          2
 
 /* USER CODE END EC */
 
